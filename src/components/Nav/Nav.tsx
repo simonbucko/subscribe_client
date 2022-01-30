fdb4478e5d1a902cc2bdfb0796a730eb2b724c23
SimonBucko <bucino.36@gmail.com>
2022-01-30 12:09:22 +0100

    removed some unused template

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navbar, NavItem, NavLink} from "react-bootstrap"
+
+const Nav = () => {
+  return <Navbar>
+    <NavItem>
+        <NavLink>
+          Home
+        </NavLink>
+      </NavItem></Navbar>;
+};
+
+export default Nav;
