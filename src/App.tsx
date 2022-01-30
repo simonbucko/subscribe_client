95bfba76d0cc52fbbc3d3a65afa5bec47cad26d1
SimonBucko <bucino.36@gmail.com>
2022-01-30 12:13:32 +0100

    sdded styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Nav/Nav';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
