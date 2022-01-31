48f8a3ec6f862873195037a68f4164a34b2ee52c
SimonBucko <bucino.36@gmail.com>
2022-01-31 11:29:00 +0100

    cahnges

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
 import {Navbar, NavItem, NavLink} from "react-bootstrap"
+import {Link} from "react-router-dom" 
 
 const Nav = () => {
-  return <Navbar>
-    <NavItem>
-        <NavLink>
-          Home
-        </NavLink>
-      </NavItem></Navbar>;
+  return (
+    <Navbar>
+      <NavItem>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+      </NavItem>
+      {localStorage.getItem("token") && (
+        <NavItem>
+        <Link to="/" className="nav-link">
+          Logout
+        </Link>
+    </NavItem>
+      )}
+    </Navbar>
+  );
 };
 
 export default Nav;
