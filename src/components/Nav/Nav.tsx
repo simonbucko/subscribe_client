ad0e1c091daaba443ce234a8eda9f7a88367e3fb
SimonBucko <bucino.36@gmail.com>
2022-02-01 07:57:35 +0100

    implemented logout functionality

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,22 @@
-import {Navbar, NavItem} from "react-bootstrap"
-import {Link} from "react-router-dom" 
+import {Navbar, NavItem, NavLink} from "react-bootstrap"
+import {Link,useNavigate} from "react-router-dom" 
 import { useContext } from "react";
 import { UserContext } from "../../context";
+import styled from "styled-components"
+
+const LeftNavContainer = styled.div`
+  margin-left: auto;
+`
 
 const Nav = () => {
 const [state,setState] = useContext(UserContext)
+const navigate = useNavigate();
+
+const handleLogout = () =>{
+  setState({data:null,loading:false,error:null})
+  localStorage.removeItem("token")
+  navigate("/")
+}
 
 console.log(state)
 
@@ -15,12 +27,15 @@ console.log(state)
             Home
           </Link>
       </NavItem>
-      {localStorage.getItem("token") && (
-        <NavItem>
-        <Link to="/" className="nav-link">
-          Logout
-        </Link>
-    </NavItem>
+      {state.data && (
+        <LeftNavContainer>
+          <NavItem>
+            <NavLink onClick={handleLogout}>
+              Logout
+            </NavLink>
+        </NavItem>
+        </LeftNavContainer>
+        
       )}
     </Navbar>
   );
