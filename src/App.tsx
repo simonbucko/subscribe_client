0393f50627c3e2cf3d6d64b3adb6dec8347d7d9e
SimonBucko <bucino.36@gmail.com>
2022-02-01 09:36:23 +0100

    implemented protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Nav/Nav';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import Articles from './pages/Articles';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
-        <Route path="/articles" element={<Articles/>}/>
+        <Route path="/articles" element={<ProtectedRoute/>}>
+          <Route path="/articles" element={<Articles/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import { UserContext } from '../context';
+
+const ProtectedRoute = () => {
+    const [state] = useContext(UserContext)
+    if(state.loading) return <div>Loading...</div>
+    // outlet is reference to children component
+    return state.data ? <Outlet/> : <Navigate to={"/"}/>;
+};
+
+export default ProtectedRoute;
