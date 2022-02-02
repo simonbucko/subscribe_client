471ece2c5b7400cbf284a4f6f3cddf10809ac1bc
SimonBucko <bucino.36@gmail.com>
2022-02-02 21:29:12 +0100

    added new page articles plan

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Nav/Nav';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import LandingPage from './pages/LandingPage';
 import Articles from './pages/Articles';
+import ArticlesPlan from './pages/ArticlesPlan';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/articles" element={<ProtectedRoute/>}>
           <Route path="/articles" element={<Articles/>}/>
+        </Route>
+        <Route path="/article-plans" element={<ProtectedRoute/>}>
+          <Route path="/article-plans" element={<ArticlesPlan/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/ArticlesPlan.tsx
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import React,{useEffect,useState} from 'react';
+import styled from 'styled-components';
+import { Container, Card } from 'react-bootstrap';
+
+const CardsContainer = styled.div`
+display: flex;
+height: 75vh;
+align-items: center;
+justify-content: center;
+`
+const CardHeader = styled.div`
+height: 30rem;
+background-color: blue;
+display: flex;
+align-items: center;
+justify-content: center
+`
+
+const PriceCircle = styled.div`
+border: 0.5rem solid white;
+width: 12.5rem;
+height: 12.5rem;
+border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+box-shadow: 0.1rem 0.1rem 1rem rgba(19,20,19,0.342)
+`
+
+const PriceText = styled.p`
+font-size: 3rem;
+color: white;
+text-shadow: 0.1rem 0.1rem 1rem rgba(19,20,19,0.342)
+`
+
+
+const ArticlesPlan = () => {
+  const [prices, setPrices] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchPrices()
+  }, []);
+
+  const fetchPrices = async() => {
+    const {data: response} = await axios.get("http://localhost:8080/subs/prices")
+    setPrices(response.data)
+  }
+  
+
+  return <Container>
+    <CardsContainer>
+      {prices.map((price:any) => (
+        <Card style={{width: "18rem", marginRight:"2rem"}}> 
+          <CardHeader>
+            <PriceCircle>
+              <PriceText>
+              ${price.unit_amount/100}
+              </PriceText>
+            </PriceCircle>
+          </CardHeader>
+        </Card>
+      ))}
+    </CardsContainer>
+  </Container>;
+};
+
+export default ArticlesPlan;
